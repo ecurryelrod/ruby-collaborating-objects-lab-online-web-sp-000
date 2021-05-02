da2a286013bb0fd62fb2b6e02b5ae10a8d64d1ab
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-02 18:41:08 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer 
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path
+  end 
+  
+  def files 
+    @files = Dir.children(@path)
+  end 
+  
+  def import(filenames)
+    filenames.each {|filename| Song.new_by_file_name(filename)}
+  end 
+end 
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist 
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.new_by_filename(filename)
+    file_array = filename.split(" - ")
+    song = self.new(file_array[1])
+   # binding.pry
+    song.artist = Artist.find_or_create_by_name(file_array[0])
+    song.artist.name
+  end 
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else 
+      self.artist.name = name
+    end 
+    
+  end 
+end 
