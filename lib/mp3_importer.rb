abde10b387288d0adf949e5301b1632159d239f9
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-02 19:37:58 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MP3Importer 
   attr_accessor :path 
   
@@ -7,6 +9,7 @@ class MP3Importer
   
   def files 
     @files = Dir.children(@path)
+    binding.pry
   end 
   
   def import
