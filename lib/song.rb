88b3fbcaabbe8e50bdca1ee21d042e578afaf84a
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-02 19:04:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,8 @@ class Song
   def self.new_by_filename(filename)
     file_array = filename.split(" - ")
     song = self.new(file_array[1])
-    binding.pry
     artist = Artist.find_or_create_by_name(file_array[0])
+    binding.pry
     artist.add_song(song)
     song
   end 
